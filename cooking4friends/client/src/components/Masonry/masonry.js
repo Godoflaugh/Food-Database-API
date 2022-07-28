9589d27efad3a9fb1335726bc4da6520ebe2a3e4
Sam <2726-sambergeland-WKCcKe@users.noreply.uci.bootcampcontent.com>
2022-07-28 10:08:55 -0700

    started recipe form input

--- cooking4friends/client/src/components/Masonry/masonry.js
+++ cooking4friends/client/src/components/Masonry/masonry.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
-import Masonry from '@mui/lab/Masonry';
+// import Masonry from '@mui/lab/Masonry';
 
 const heights = [150, 30, 90, 70, 110, 150, 130, 80, 50, 90, 100, 150, 30, 50, 80];
 
---
+++ cooking4friends/client/src/components/Recipeform/recipeform.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+export default function recipeForm() {
+
+
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+
+        />
+        <TextField
+          id="outlined-textarea"
+          label="Multiline Placeholder"
+          placeholder="Placeholder"
+          multiline
+        />
+        <TextField
+          id="outlined-multiline-static"
+          label="Multiline"
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+        />
+      </div>
+
+
+    </Box>
+  );
+
+
+}
